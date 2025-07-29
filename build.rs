cef1cad1881b01db26e8715fc77acc2303a06f85
SebastianRojas6 <sebastian.rojas6@unmsm.edu.pe>
2025-07-29 13:10:21 -0500

    Feat(cards) : Cards added

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
 fn main() {
-    slint_build::compile("ui/background.slint").expect("Slint build failed");
-    slint_build::compile("ui/menu.slint").expect("Slint build failed");
+    slint_build::compile("ui/main.slint").expect("Slint build failed");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let main_window = Menu::new()?;
 
-    main_window.run()
+    let menu = Menu::new()?; 
+    let photos = Photos::new()?;
+    let cards = Cards::new()?;
+    let game = Game::new()?;
+
+
+    let mut x = String::new();
+    std::io::stdin().read_line(&mut x).expect("Error");
+    let x_int : i32 = x.trim().parse().expect("Error"); 
+
+    if x_int == 1 {
+
+        menu.run()
+    } 
+
+    else if x_int == 2{
+        photos.run()
+    }
+
+    else if x_int == 3{
+        cards.run()
+    }
+
+    else if x_int == 4 {
+        game.run()
+    }
+
+    else{
+        menu.run()
+    }
+
 }
